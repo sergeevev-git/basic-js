ceeb647e98aa3300c6a8c69d7e9d6b707176f607
sergeevev <sergeevevit@gmail.com>
2021-09-11 23:34:13 +0500

    what-season

--- src/what-season.js
+++ src/what-season.js
@@ -1,17 +1,45 @@
-import { NotImplementedError } from '../extensions/index.js';
+import { NotImplementedError } from "../extensions/index.js";
 
 /**
  * Extract season from given date and expose the enemy scout!
- * 
+ *
  * @param {Date | FakeDate} date real or fake date
  * @returns {String} time of the year
- * 
+ *
  * @example
- * 
+ *
  * getSeason(new Date(2020, 02, 31)) => 'spring'
- * 
+ *
  */
-export default function getSeason(/* date */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+
+function isValidDate(value) {
+    var dateWrapper = new Date(value);
+    return !isNaN(dateWrapper.getDate());
+}
+
+export default function getSeason(date) {
+    // throw new NotImplementedError('Not implemented');
+    // remove line with error and write your code here
+    if (!date) {
+        return "Unable to determine the time of year!";
+    }
+    try {
+        date.toJSON();
+    } catch (e) {
+        return "Invalid date!";
+    }
+
+    if (
+        date.getMonth() === 11 ||
+        date.getMonth() === 0 ||
+        date.getMonth() === 1
+    ) {
+        return "winter";
+    } else if (date.getMonth() >= 2 && date.getMonth() <= 4) {
+        return "spring";
+    } else if (date.getMonth() >= 5 && date.getMonth() <= 7) {
+        return "summer";
+    } else if (date.getMonth() >= 8 && date.getMonth() <= 10) {
+        return "fall";
+    }
 }
